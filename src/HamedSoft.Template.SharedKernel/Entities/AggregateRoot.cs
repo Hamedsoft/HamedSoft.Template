6e3c0887543951e368e260cd79ea64350f65e035
HamedSoft <HamedSoft@DESKTOP-1I7VG7S>
2026-07-09 18:45:02 +0330

    [add] | [add entity and agrregateroot to shared]

---
+++ src/HamedSoft.Template.SharedKernel/Entities/AggregateRoot.cs
@@ -0,0 +1,22 @@
+﻿namespace HamedSoft.Template.SharedKernel.Entities;
+
+public abstract class AggregateRoot : Entity
+{
+    private readonly List<object> _domainEvents = new();
+
+
+    public IReadOnlyCollection<object> DomainEvents
+        => _domainEvents.AsReadOnly();
+
+
+    protected void AddDomainEvent(object domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+}
---
+++ src/HamedSoft.Template.SharedKernel/Events/IDomainEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamedSoft.Template.SharedKernel.Events
+{
+    internal interface IDomainEvent
+    {
+    }
+}
