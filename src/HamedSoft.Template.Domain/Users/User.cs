c6f442f72add0669fd81d608c481b610b7ea53c6
HamedSoft <HamedSoft@DESKTOP-1I7VG7S>
2026-07-09 19:07:38 +0330

    [Add] | [Add Domain entity]

---
+++ src/HamedSoft.Template.Domain/Users/User.cs
@@ -0,0 +1,26 @@
+﻿using HamedSoft.Template.SharedKernel.Entities;
+
+namespace HamedSoft.Template.Domain.Users;
+
+public class User : AggregateRoot
+{
+    private User()
+    {
+        FirstName = string.Empty;
+        LastName = string.Empty;
+    }
+
+
+    public User(
+        string firstName,
+        string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+
+    public string FirstName { get; private set; }
+
+    public string LastName { get; private set; }
+}
