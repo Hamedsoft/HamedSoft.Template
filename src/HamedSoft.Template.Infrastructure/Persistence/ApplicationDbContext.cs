0c0eae04b881229dfa833d8bacc48efc0fd108e0
HamedSoft <HamedSoft@DESKTOP-1I7VG7S>
2026-07-10 06:50:15 +0330

    [Add] | [Add identity user]

--- src/HamedSoft.Template.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/HamedSoft.Template.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HamedSoft.Template.Domain.Users;
+using HamedSoft.Template.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
-namespace HamedSoft.Template.Infrastructure.Persistence
+namespace HamedSoft.Template.Infrastructure.Persistence;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
 {
-    internal class ApplicationDbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
+    }
+
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
     }
 }
---
+++ src/HamedSoft.Template.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using HamedSoft.Template.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HamedSoft.Template.Infrastructure.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.FirstName)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(x => x.LastName)
+            .HasMaxLength(100)
+            .IsRequired();
+    }
+}
