5ab937d77ffaed7a0c12e001ceb459999f434da5
HamedSoft <HamedSoft@DESKTOP-1I7VG7S>
2026-07-10 06:41:12 +0330

    [Add] | [Add Identity user and role]

--- src/HamedSoft.Template.Infrastructure/Identity/ApplicationRole.cs
+++ src/HamedSoft.Template.Infrastructure/Identity/ApplicationRole.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
 
-namespace HamedSoft.Template.Infrastructure.Identity
+namespace HamedSoft.Template.Infrastructure.Identity;
+
+public class ApplicationRole : IdentityRole<Guid>
 {
-    internal class ApplicationRole
-    {
-    }
 }
--- src/HamedSoft.Template.Infrastructure/Identity/ApplicationUser.cs
+++ src/HamedSoft.Template.Infrastructure/Identity/ApplicationUser.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
 
-namespace HamedSoft.Template.Infrastructure.Identity
+namespace HamedSoft.Template.Infrastructure.Identity;
+
+public class ApplicationUser : IdentityUser<Guid>
 {
-    internal class ApplicationUser
-    {
-    }
 }
