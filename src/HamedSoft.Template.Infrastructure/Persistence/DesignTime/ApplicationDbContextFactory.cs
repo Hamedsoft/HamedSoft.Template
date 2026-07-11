0155412c4e4fe25f72fc090628898a6963956c7e
HamedSoft <H.Ziaei@Gmail.com>
2026-07-11 11:58:17 +0330

    [Add] | [Application db context factory]

---
+++ src/HamedSoft.Template.Infrastructure/Persistence/DesignTime/ApplicationDbContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace HamedSoft.Template.Infrastructure.Persistence.DesignTime;
+
+public class ApplicationDbContextFactory
+    : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var path = Path.Combine(
+            Directory.GetCurrentDirectory(),
+            "../HamedSoft.Template.Web");
+
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(path)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        optionsBuilder.UseSqlServer(
+            configuration.GetConnectionString("DefaultConnection"));
+
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
