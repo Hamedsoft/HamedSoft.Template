1c0f4a65e224e32c33f73462f9aaf07c8b57deb3
HamedSoft <H.Ziaei@Gmail.com>
2026-08-10 09:10:13 +0330

    [Fix] | [Fix register and login]

--- src/HamedSoft.Template.Web/ViewModels/Auth/LoginViewModel.cs
+++ src/HamedSoft.Template.Web/ViewModels/Auth/LoginViewModel.cs
@@ -4,11 +4,13 @@ namespace HamedSoft.Template.Web.ViewModels.Auth;
 
 public sealed class LoginViewModel
 {
-    [Required]
+    [Required(ErrorMessage = "{0} را وارد کنید")]
+    [Display(Name = "نام کاربری")]
     public string UserName { get; set; } = string.Empty;
 
-    [Required]
+    [Required(ErrorMessage = "{0} را وارد کنید")]
     [DataType(DataType.Password)]
+    [Display(Name = "کلمه عبور")]
     public string Password { get; set; } = string.Empty;
 
     [Display(Name ="من را به خاطر بسپار")]
--- src/HamedSoft.Template.Web/ViewModels/Auth/RegisterViewModel.cs
+++ src/HamedSoft.Template.Web/ViewModels/Auth/RegisterViewModel.cs
@@ -4,26 +4,26 @@ namespace HamedSoft.Template.Web.ViewModels.Auth;
 
 public class RegisterViewModel
 {
-    [Required]
+    [Required(ErrorMessage ="{0} را وارد کنید")]
     [Display(Name = "نام کاربری")]
     public string UserName { get; set; } = string.Empty;
 
-    [Required]
+    [Required(ErrorMessage = "{0} را وارد کنید")]
     [DataType(DataType.Password)]
     [Display(Name = "کلمه عبور")]
     public string Password { get; set; } = string.Empty;
 
-    [Required]
+    [Required(ErrorMessage = "{0} را وارد کنید")]
     [DataType(DataType.Password)]
     [Compare(nameof(Password))]
     [Display(Name = "تکرار کلمه عبور")]
     public string ConfirmPassword { get; set; } = string.Empty;
 
-    [Required]
+    [Required(ErrorMessage = "{0} را وارد کنید")]
     [Display(Name = "نام")]
     public string FirstName { get; set; } = string.Empty;
 
-    [Required]
+    [Required(ErrorMessage = "{0} را وارد کنید")]
     [Display(Name = "نام خانوادگی")]
     public string LastName { get; set; } = string.Empty;
 }
