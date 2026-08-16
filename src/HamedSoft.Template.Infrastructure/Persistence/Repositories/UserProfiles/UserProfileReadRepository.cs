43bfd3a4334b3380fab209b49877fd2c2e136423
HamedSoft <H.Ziaei@Gmail.com>
2026-08-16 18:15:09 +0330

    [Fix] | [Clean Code]

--- src/HamedSoft.Template.Infrastructure/Persistence/Repositories/UserProfiles/UserProfileReadRepository.cs
+++ src/HamedSoft.Template.Infrastructure/Persistence/Repositories/UserProfiles/UserProfileReadRepository.cs
@@ -5,38 +5,25 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HamedSoft.Template.Infrastructure.Persistence.Repositories.UserProfiles;
 
-internal sealed class UserProfileReadRepository
-    : IUserProfileReadRepository
+internal sealed class UserProfileReadRepository : IUserProfileReadRepository
 {
     private readonly ApplicationDbContext _context;
 
-    public UserProfileReadRepository(
-        ApplicationDbContext context)
+    public UserProfileReadRepository(ApplicationDbContext context)
     {
         _context = context;
     }
 
-    public async Task<UserProfile?> GetByIdAsync(
-        UserProfileId id,
-        CancellationToken cancellationToken = default)
+    public async Task<UserProfile?> GetByIdAsync(UserProfileId id, CancellationToken cancellationToken = default)
     {
-        return await _context.UserProfiles
-            .AsNoTracking()
-            .FirstOrDefaultAsync(
-                x => x.Id == id,
-                cancellationToken);
+        return await _context.UserProfiles.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
     }
 
-    public async Task<IReadOnlyList<UserProfile>> GetByIdsAsync(
-        IReadOnlyCollection<UserProfileId> ids,
-        CancellationToken cancellationToken = default)
+    public async Task<IReadOnlyList<UserProfile>> GetByIdsAsync(IReadOnlyCollection<UserProfileId> ids, CancellationToken cancellationToken = default)
     {
         if (ids.Count == 0)
             return Array.Empty<UserProfile>();
 
-        return await _context.UserProfiles
-            .AsNoTracking()
-            .Where(x => ids.Contains(x.Id))
-            .ToListAsync(cancellationToken);
+        return await _context.UserProfiles.AsNoTracking().Where(x => ids.Contains(x.Id)).ToListAsync(cancellationToken);
     }
 }
